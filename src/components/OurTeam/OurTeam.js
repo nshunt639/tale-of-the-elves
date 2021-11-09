6c41d32cac1e3d689e61f56af105130a95f8b225
nshunt639 <nshunt639@gmail.com>
2021-11-09 18:18:44 +0200

    feat: update team members

--- src/components/OurTeam/OurTeam.js
+++ src/components/OurTeam/OurTeam.js
@@ -6,10 +6,10 @@ import useStyles from './OurTeam.styles'
 const TEAM_MEMBERS = [
     {name: 'Name 1', title: 'Title 1', image: '/images/team/1.gif', twitter: '', discord: ''},
     {name: 'Name 2', title: 'Title 2', image: '/images/team/2.gif', twitter: '', discord: ''},
-    {name: 'Name 3', title: 'Title 3', image: '/images/team/3.gif', twitter: '', discord: ''},
+    {name: 'Name 3', title: 'Title 3', image: '/images/team/3.png', twitter: '', discord: ''},
     {name: 'Name 4', title: 'Title 4', image: '/images/team/4.gif', twitter: '', discord: ''},
     {name: 'Name 5', title: 'Title 5', image: '/images/team/5.gif', twitter: '', discord: ''},
-    {name: 'Name 6', title: 'Title 6', image: '/images/team/6.gif', twitter: '', discord: ''}
+    {name: 'Name 6', title: 'Title 6', image: '/images/team/6.png', twitter: '', discord: ''}
 ]
 const OurTeam = () => {
     const classes = useStyles()
